971f665f8c687dd7c1cff6900a1fc5b24e78361b
baojianhang1 <baojianhang1@360.cn>
2022-11-17 14:00:59 +0800

    修改本地构建脚本

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PWD=$(pwd)
+ROOT=$(pwd)
 
 echo "请选择构建设备："
 echo "1. AX1800"
@@ -18,19 +18,18 @@ case $input in
 esac
 
 #clone source tree
-git clone https://github.com/gl-inet/gl-infra-builder.git $PWD/gl-infra-builder
-cp -r $PWD/*.yml $PWD/gl-infra-builder/profiles
-cd $PWD/gl-infra-builder
+git clone https://github.com/gl-inet/gl-infra-builder.git $ROOT/gl-infra-builder
+cp -r $ROOT/*.yml $ROOT/gl-infra-builder/profiles
+cd $ROOT/gl-infra-builder
 #setup
 python3 setup.py -c config-wlan-ap-5.4.yml
 
 cd wlan-ap/openwrt
-./scripts/gen_config.py  $PWD/gl-infra-builder/profiles/glinet_$DEVICE glinet_depends
+./scripts/gen_config.py  $ROOT/gl-infra-builder/profiles/glinet_$DEVICE-5-4 glinet_depends
 
-git clone https://github.com/gl-inet/glinet4.x.git -b main $PWD/glinet
-./scripts/feeds update -a
-./scripts/feeds install -a
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
 make defconfig
 
 
-make -j$(expr $(nproc) + 1) GL_PKGDIR=$PWD/glinet/ipq60xx/ V=s
+make -j$(expr $(nproc) + 1)  V=s
